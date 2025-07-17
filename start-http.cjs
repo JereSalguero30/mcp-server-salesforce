27dcd402d2d2648d9f98e2a70650fb9edb4ec6f3
Jeremias Salguero <jesalgue@emeal.nttdata.com>
2025-07-16 23:47:33 -0300

    query-endpoint

--- start-http.cjs
+++ start-http.cjs
@@ -1,10 +1,11 @@
 require('dotenv').config();
-
 const { createHttpServer } = require('./dist/tools/http-server');
 
+const port = process.env.PORT || 3000; // <- CAMBIO CLAVE AQUÍ
 
-createHttpServer().then(() => {
-  console.log('✅ MCP server HTTP listening on port 3000');
+createHttpServer({ port }).then(() => {
+  console.log('✅ MCP server HTTP listening on port', port);
   console.log("USERNAME:", process.env.SALESFORCE_USERNAME);
   console.log("PASSWORD:", process.env.SALESFORCE_PASSWORD);
 });
+
